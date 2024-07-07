ff00f6a530c435690c2743506fca0c31d58de9cb
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-07-07 13:45:02 +0200

    Visual Tweaks for Restaurants

--- backend/src/main/java/de/sky/meal/ordering/mealordering/endpoint/RestaurantController.java
+++ backend/src/main/java/de/sky/meal/ordering/mealordering/endpoint/RestaurantController.java
@@ -8,19 +8,51 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Controller
 public class RestaurantController implements generated.sky.meal.ordering.rest.api.RestaurantApi {
-    private final List<Restaurant> restaurants = IntStream.range(0, 25)
-            .mapToObj(idx ->
-                    Restaurant.builder()
-                            .id(UUID.randomUUID())
-                            .name("Restaurant #" + idx)
-                            .build()
-            )
-            .collect(Collectors.toCollection(ArrayList::new));
+    private final List<Restaurant> restaurants = new ArrayList<>();
+
+
+    {
+        restaurants.add(
+                Restaurant.builder()
+                        .id(UUID.randomUUID())
+                        .name("Chok Dee Thaiküche")
+                        .style("thailändisch/asiatisch")
+                        .kind("Abholen/Sitzen")
+                        .phone("0221 / 47 68 48 99")
+                        .website("https://www.chok-dee-rath.de/")
+                        .shortDescription("Sehr guter Thai zum Abholen")
+                        .description("Sehr guter Thai\n".repeat(10))
+                        .address(
+                                generated.sky.meal.ordering.rest.model.Address.builder()
+                                        .street("Rösrather Straße")
+                                        .housenumber("575")
+                                        .postal("51107")
+                                        .city("Köln")
+                                        .build()
+                        )
+                        .build()
+        );
+
+        restaurants.add(
+                Restaurant.builder()
+                        .id(UUID.randomUUID())
+                        .name("Gaststätte Jägerhof")
+                        .style("gutbürgerlich/deutsch")
+                        .style("Sitzen")
+                        .phone("0221 866831")
+                        .address(generated.sky.meal.ordering.rest.model.Address.builder()
+                                .street("Rösrather Str.")
+                                .housenumber("661")
+                                .postal("51107")
+                                .city("Köln")
+                                .build())
+                        .build()
+        );
+    }
+
 
     @Override
     public ResponseEntity<Restaurant> createRestaurant(Restaurant restaurant) {
--- frontend/src/components/restaurant/RestaurantsOverview.tsx
+++ frontend/src/components/restaurant/RestaurantsOverview.tsx
@@ -1,36 +1,77 @@
 import {Restaurant} from "../../../build/generated-ts/api/api.ts";
-import {Button, Card, CardActions, CardContent, Divider, Fab, Paper, Typography} from "@mui/material";
+import {Avatar, Button, Card, CardActions, CardContent, Paper, Stack, Typography} from "@mui/material";
 import styled from "styled-components";
 import {api} from "../../api/api.ts";
-import AddIcon from '@mui/icons-material/Add';
-import {useCallback, useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import LoadingIndicator from "../../utils/LoadingIndicator.tsx";
-import {useNavigate} from "react-router-dom";
 
-function RestaurantCard({restaurant}: { restaurant: Restaurant }) {
+type RestaurantCardProps = {
+  restaurant: Restaurant;
+  isNew?: boolean;
+};
+
+function RestaurantCard({restaurant, isNew}: RestaurantCardProps) {
+  const avatar = useMemo(() => {
+    if (isNew)
+      return null;
+
+    return restaurant.name.split(' ')
+      .slice(0, 2)
+      .map(w => w.charAt(0))
+      .map(c => c.toUpperCase())
+      .join('');
+  }, [restaurant.name, isNew]);
+
+  const color = useMemo(() => {
+    if (isNew)
+      return null;
+
+    let hash = 0;
+    for (let i = 0; i < restaurant.name.length; i++) {
+      const c = restaurant.name.charCodeAt(i);
+      hash = (hash << 5) - hash + c;
+      hash |= 0;
+    }
+    return Math.abs(hash).toString(16);
+  }, [restaurant.name, isNew]);
+
   return <Card sx={{minWidth: 500}} elevation={8}>
     <CardContent>
-      <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
-        Restaurant
-      </Typography>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Avatar sizes="sm" sx={{bgcolor: `#${color}`}}>
+          {avatar}
+        </Avatar>
+
+        {
+          [restaurant.kind, restaurant.style]
+            .map((text, idx) => {
+                return <Typography key={text || idx} sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                  {text}
+                </Typography>;
+              }
+            )
+        }
+      </Stack>
 
       <Typography variant="h5" component="div">
         {restaurant.name}
       </Typography>
 
-      <Typography sx={{mb: 1.5}} color="text.secondary">
-        thailändisch/asiatisch
-      </Typography>
-
-      <Typography variant="body2">
-        Bemerkung zum Restaurant
+      <Typography variant="body2" color="text.secondary">
+        {restaurant.shortDescription}
       </Typography>
     </CardContent>
 
     <CardActions>
-      <Button size="small" href={`/restaurant/${restaurant.id}`} variant="contained">
-        Anschauen
-      </Button>
+      <Stack sx={{width: '100%'}} direction="row" justifyContent="space-between">
+        <Button>
+          {restaurant.phone}
+        </Button>
+
+        <Button size="small" href={`/restaurant/${restaurant.id}`} variant="contained">
+          {isNew ? 'Neu anlegen' : 'Bearbeiten'}
+        </Button>
+      </Stack>
     </CardActions>
   </Card>
 }
@@ -39,35 +80,26 @@ export default function RestaurantsOverview() {
   const [restaurants, setRestaurants] = useState<Restaurant[] | null>(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      api.restaurants.fetchRestaurants()
-        .then(res => setRestaurants(res.data))
-    }, 500)
+    api.restaurants.fetchRestaurants()
+      .then(res => setRestaurants(res.data))
   }, []);
-
-  const navigate = useNavigate();
-
-  const onNewRestaurant = useCallback(() => {
-    console.log("new restaruant")
-
-    navigate({pathname: `./new-restaurant`,}, {relative: 'path'});
-  }, [navigate]);
 
   return <Paper elevation={8}>
     <Typography variant="h3">
       Restaurants
     </Typography>
 
-    <Fab color="primary"
-         onClick={onNewRestaurant}>
-      <AddIcon/>
-    </Fab>
-
-    <Divider dir="row"/>
-
     <LoadingIndicator isLoading={restaurants === null}>
       <SFlexGrid>
         {restaurants && restaurants.map(r => <RestaurantCard key={r.id} restaurant={r}/>)}
+
+        <RestaurantCard isNew={true}
+                        restaurant={{
+                          id: 'new-restaurant',
+                          name: 'Neues Restaurant',
+                          style: 'Neu',
+                          shortDescription: 'Erstelle neues Restaurant'
+                        }}/>
       </SFlexGrid>
     </LoadingIndicator>
   </Paper>;
