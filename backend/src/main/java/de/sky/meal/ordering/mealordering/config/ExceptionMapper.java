ce2637ac24f0a9ecef9bc8b91c2e43ebe6ce02ad
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-07-13 11:37:24 +0200

    Changed Error handling in API and Server

--- backend/src/main/java/de/sky/meal/ordering/mealordering/config/ExceptionMapper.java
+++ backend/src/main/java/de/sky/meal/ordering/mealordering/config/ExceptionMapper.java
@@ -1,5 +1,7 @@
 package de.sky.meal.ordering.mealordering.config;
 
+import generated.sky.meal.ordering.rest.model.ErrorObject;
+import jakarta.ws.rs.ClientErrorException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,10 +12,27 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class ExceptionMapper {
     @ExceptionHandler(Throwable.class)
-    public ResponseEntity<String> handleCustomException(Throwable ex, WebRequest request) {
+    public ResponseEntity<ErrorObject> handleCustomException(Throwable ex, WebRequest request) {
         log.error("Critical Error in {}", request, ex);
 
         return ResponseEntity.internalServerError()
-                .build();
+                .body(
+                        ErrorObject.builder()
+                                .message(ex.getMessage())
+                                .build()
+                );
     }
+
+    @ExceptionHandler(ClientErrorException.class)
+    public ResponseEntity<ErrorObject> handleCustomException(ClientErrorException ex, WebRequest request) {
+        log.error("Client Error in {}", request, ex);
+
+        return ResponseEntity.status(ex.getResponse().getStatus())
+                .body(
+                        ErrorObject.builder()
+                                .message(ex.getMessage())
+                                .build()
+                );
+    }
+
 }
