7e367799cb80c87c9fe9e8d5cca543b2e8e10abc
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-07-17 16:48:31 +0200

    Minor Visuals

--- frontend/src/components/restaurant/RestaurantEditor.tsx
+++ frontend/src/components/restaurant/RestaurantEditor.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import {Restaurant, RestaurantPatch} from "../../../build/generated-ts/api/index.ts";
 import {Button, Divider, Stack, TextField, Typography} from "@mui/material";
 import MenuPageEditor from "./MenuPageEditor.tsx";
+import LoadingIndicator from "../../utils/LoadingIndicator.tsx";
 
 type RestaurantEditorProps = {
   restaurant: Restaurant;
@@ -13,6 +14,7 @@ type RestaurantEditorProps = {
 };
 
 export default function RestaurantEditor({restaurant, isNew, onRefresh}: RestaurantEditorProps) {
+  const [isWorking, setIsWorking] = useState(false);
   const [touched, setTouched] = useState(false);
 
   const [name, setName] = useState(restaurant.name || '');
@@ -67,6 +69,8 @@ export default function RestaurantEditor({restaurant, isNew, onRefresh}: Restaur
       },
     };
 
+    setIsWorking(true);
+
     (isNew
         ? () => api.restaurants.createRestaurant(newRestaurant)
         : () => api.restaurants.updateRestaurant(restaurant.id, newRestaurant)
@@ -78,133 +82,137 @@ export default function RestaurantEditor({restaurant, isNew, onRefresh}: Restaur
         navigate({pathname: `/restaurant/${rest.id}`}, {replace: true});
       })
       .then(() => onRefresh?.()) // to explicitly trigger a reload of the parent, to see changes coming from the server
+      .finally(()=>setIsWorking(false))
   }, [name, style, kind, phone, website, email, shortDescription, description, street, housenumber, postal, city, isNew, navigate, menuPagesOnSave, restaurant?.id, onRefresh]);
 
   const nameIsValid = !!name && !!name.trim();
   const isValid = nameIsValid;
 
-  return <Stack spacing={2}>
-    <Typography variant="h4">{isNew ? 'Neues' : ''} Restaurant</Typography>
-
+  return <LoadingIndicator isLoading={isWorking}>
     <Stack spacing={2}>
-      <Stack direction="row" spacing={2} justifyContent="space-between">
+      <Typography variant="h4">{isNew ? 'Neues' : ''} Restaurant</Typography>
+
+      <Stack spacing={2}>
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <SStack spacing={2}>
+            <STextField size="small"
+                        label="Name"
+                        value={name}
+                        onChange={e => {
+                          setName(e.target.value);
+                          setTouched(true)
+                        }}
+                        error={!nameIsValid}
+                        helperText={!nameIsValid && "Name muss vorhanden sein"}
+            />
+            <STextField size="small" label="Style" value={style} onChange={e => {
+              setStyle(e.target.value);
+              setTouched(true)
+            }}/>
+            <STextField size="small" label="Typ" value={kind} onChange={e => {
+              setKind(e.target.value);
+              setTouched(true)
+            }}/>
+          </SStack>
+
+          <SStack spacing={2} justifyContent="space-between">
+            <STextField size="small" label="Phone" value={phone} onChange={e => {
+              setPhone(e.target.value);
+              setTouched(true)
+            }}/>
+            <STextField size="small" label="Website" value={website} onChange={e => {
+              setWebsite(e.target.value);
+              setTouched(true)
+            }}/>
+            <STextField size="small" label="Email" value={email} onChange={e => {
+              setEmail(e.target.value);
+              setTouched(true)
+            }}/>
+          </SStack>
+        </Stack>
+
+        <Divider/>
+
+        <Stack spacing={2} justifyContent="space-between">
+          <SStack direction="row" spacing={2} justifyContent="space-between">
+            <STextField size="small" label="Street" value={street} onChange={e => {
+              setStreet(e.target.value);
+              setTouched(true)
+            }}/>
+            <TextField size="small" label="Housenumber" value={housenumber}
+                       onChange={e => {
+                         setHousenumber(e.target.value);
+                         setTouched(true)
+                       }}/>
+          </SStack>
+
+          <SStack direction="row" spacing={2} justifyContent="space-between">
+            <TextField size="small" label="Postal" value={postal} onChange={e => {
+              setPostal(e.target.value);
+              setTouched(true)
+            }}/>
+            <STextField size="small" label="City" value={city} onChange={e => {
+              setCity(e.target.value);
+              setTouched(true)
+            }}/>
+          </SStack>
+        </Stack>
+
+        <Divider/>
+
         <SStack spacing={2}>
-          <STextField size="small"
-                      label="Name"
-                      value={name}
-                      onChange={e => {
-                        setName(e.target.value);
-                        setTouched(true)
-                      }}
-                      error={!nameIsValid}
-                      helperText={!nameIsValid && "Name muss vorhanden sein"}
-          />
-          <STextField size="small" label="Style" value={style} onChange={e => {
-            setStyle(e.target.value);
-            setTouched(true)
-          }}/>
-          <STextField size="small" label="Typ" value={kind} onChange={e => {
-            setKind(e.target.value);
-            setTouched(true)
-          }}/>
-        </SStack>
-
-        <SStack spacing={2} justifyContent="space-between">
-          <STextField size="small" label="Phone" value={phone} onChange={e => {
-            setPhone(e.target.value);
-            setTouched(true)
-          }}/>
-          <STextField size="small" label="Website" value={website} onChange={e => {
-            setWebsite(e.target.value);
-            setTouched(true)
-          }}/>
-          <STextField size="small" label="Email" value={email} onChange={e => {
-            setEmail(e.target.value);
-            setTouched(true)
-          }}/>
-        </SStack>
-      </Stack>
-
-      <Divider/>
-
-      <Stack spacing={2} justifyContent="space-between">
-        <SStack direction="row" spacing={2} justifyContent="space-between">
-          <STextField size="small" label="Street" value={street} onChange={e => {
-            setStreet(e.target.value);
-            setTouched(true)
-          }}/>
-          <TextField size="small" label="Housenumber" value={housenumber}
+          <TextField size="small"
+                     label="Short Description"
+                     value={shortDescription}
                      onChange={e => {
-                       setHousenumber(e.target.value);
+                       setShortDescription(e.target.value);
                        setTouched(true)
                      }}/>
+          <TextField size="small"
+                     label="Description"
+                     value={description}
+                     onChange={e => {
+                       setDescription(e.target.value);
+                       setTouched(true)
+                     }}
+                     multiline={true}
+                     rows={5}/>
         </SStack>
 
-        <SStack direction="row" spacing={2} justifyContent="space-between">
-          <TextField size="small" label="Postal" value={postal} onChange={e => {
-            setPostal(e.target.value);
-            setTouched(true)
-          }}/>
-          <STextField size="small" label="City" value={city} onChange={e => {
-            setCity(e.target.value);
-            setTouched(true)
-          }}/>
-        </SStack>
+        <Divider/>
+
+        <MenuPageEditor restaurant={restaurant}
+                        onChange={() => setTouched(true)}
+                        onInit={handleInit}/>
       </Stack>
 
-      <Divider/>
-
-      <SStack spacing={2}>
-        <TextField size="small"
-                   label="Short Description"
-                   value={shortDescription}
-                   onChange={e => {
-                     setShortDescription(e.target.value);
-                     setTouched(true)
-                   }}/>
-        <TextField size="small"
-                   label="Description"
-                   value={description}
-                   onChange={e => {
-                     setDescription(e.target.value);
-                     setTouched(true)
-                   }}
-                   multiline={true}
-                   rows={5}/>
-      </SStack>
-
-      <Divider/>
-
-      <MenuPageEditor restaurant={restaurant}
-                      onChange={() => setTouched(true)}
-                      onInit={handleInit}/>
-    </Stack>
-
-    <SButtonsFloat>
-      <Button onClick={onDelete}
-              color="error"
-              variant="contained">
-        Löschen
-      </Button>
-
-      <Stack direction="row" spacing={2}>
-        <Button onClick={onBack}
-                href="/restaurant"
-                color="warning"
+      <SButtonsFloat>
+        <Button onClick={onDelete}
+                color="error"
                 variant="contained">
-          Zurück
+          Löschen
         </Button>
 
-        <Button onClick={onSave}
-                color="primary"
-                variant="contained"
-                disabled={!(isValid && touched)}>
-          Speichern
-        </Button>
-      </Stack>
-    </SButtonsFloat>
-  </Stack>;
+        <Stack direction="row" spacing={2}>
+          <Button onClick={onBack}
+                  href="/restaurant"
+                  color="warning"
+                  variant="contained">
+            Zurück
+          </Button>
+
+          <Button onClick={onSave}
+                  color="primary"
+                  variant="contained"
+                  disabled={!(isValid && touched)}>
+            Speichern
+          </Button>
+        </Stack>
+      </SButtonsFloat>
+    </Stack>
+  </LoadingIndicator>;
 }
+
 const STextField = styled(TextField)`
     width: 100%;
 `;
