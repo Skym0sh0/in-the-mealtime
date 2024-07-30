dbaf5b6d28b2dad95f91ffdfb810f2150c837dbf
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-07-30 18:04:06 +0200

    feat: #30: Removed old API

--- frontend/src/utils/ApiAccessContext.tsx
+++ frontend/src/utils/ApiAccessContext.tsx
@@ -1,28 +1,37 @@
-import {createContext, ReactNode, useContext} from "react";
-import {api} from "../api/api.ts";
-import {OrderApi, RestaurantApi} from "../../build/generated-ts/api";
+import {createContext, ReactNode, useContext, useState} from "react";
+import {Configuration, OrderApi, RestaurantApi} from "../../build/generated-ts/api";
+import axios from "axios";
+import {v4 as uuidv4} from "uuid";
+
+const restConfig = new Configuration({
+  basePath: import.meta.env.VITE_APP_CONFIG_BACKEND_URL || '',
+});
+
+axios.interceptors.request.use((config) => {
+  config.headers['X-Correlation-ID'] = uuidv4();
+  return config;
+});
 
 export interface ApiAccessContextType {
   restaurantApi: RestaurantApi;
   orderApi: OrderApi;
 }
 
-const ApiAccessContext = createContext<ApiAccessContextType>({
-  restaurantApi: api.restaurants,
-  orderApi: api.orders,
-});
+const defaultApiAccess: ApiAccessContextType = {
+  restaurantApi: new RestaurantApi(restConfig),
+  orderApi: new OrderApi(restConfig),
+}
+
+const ApiAccessContext = createContext<ApiAccessContextType>(defaultApiAccess);
 
 export function useApiAccess() {
   return useContext(ApiAccessContext)
 }
 
 export function ApiAccessProvider({children}: { children?: ReactNode }) {
-  const ctx: ApiAccessContextType = {
-    restaurantApi: api.restaurants,
-    orderApi: api.orders,
-  };
+  const [api, _] = useState(() => defaultApiAccess);
 
-  return <ApiAccessContext.Provider value={ctx}>
+  return <ApiAccessContext.Provider value={api}>
     {children}
   </ApiAccessContext.Provider>
 }
