8aab4d6e359d8114dc671f48845c2708ca7ecb9a
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-07-31 01:53:43 +0200

    feat: #30: Added Snackbars as Notifications

--- frontend/src/utils/NotificationContext.tsx
+++ frontend/src/utils/NotificationContext.tsx
@@ -1,12 +1,18 @@
-import {createContext, ReactNode, useCallback, useContext} from "react";
+import {createContext, ReactNode, useCallback, useContext, useState} from "react";
+import {Alert, Snackbar} from "@mui/material";
+import {AlertColor} from "@mui/material/Alert/Alert";
+import {v4 as uuidv4} from "uuid";
 
 export interface NotificationContext {
-  notify: (message: string) => void;
+  notifyInfo: (message: string) => void;
+  notifySuccess: (message: string) => void;
   notifyError: (message: string, error?: Error) => void;
 }
 
 const NotificationContext = createContext<NotificationContext>({
-  notify: () => {
+  notifySuccess: () => {
+  },
+  notifyInfo: () => {
   },
   notifyError: () => {
   },
@@ -16,19 +22,60 @@ export function useNotification() {
   return useContext(NotificationContext);
 }
 
+type Notification = {
+  id: string;
+  text: string;
+  type: AlertColor;
+}
+
 export function NotificationContextProvider({children}: { children?: ReactNode }) {
-  const notify = useCallback((message: string) => {
-    console.log("notification", message)
+  const [notifications, setNotifications] = useState<Notification[]>([])
+
+  const addNotification = useCallback((text: string, type: AlertColor) => {
+    setNotifications(prev => [...prev, {
+      id: uuidv4(),
+      text: text,
+      type: type,
+    }])
   }, []);
 
+  const notifySuccess = useCallback((message: string) => {
+    addNotification(message, "success");
+  }, [addNotification]);
+
+  const notifyInfo = useCallback((message: string) => {
+    addNotification(message, "info");
+  }, [addNotification]);
+
   const notifyError = useCallback((message: string, error?: Error) => {
-    console.error("notification", message, error)
+    addNotification(`${message} (${error?.message})`, 'error')
+  }, [addNotification]);
+
+  const handleClose = useCallback((id: string) => {
+    setNotifications(prev => prev.filter(n => n.id !== id))
   }, []);
 
   return <NotificationContext.Provider value={{
-    notify: notify,
+    notifySuccess: notifySuccess,
+    notifyInfo: notifyInfo,
     notifyError: notifyError
   }}>
     {children}
+
+    {notifications.length > 0 &&
+      <div>
+        {notifications.slice(0, 1)
+          .map((notification) => (
+            <Snackbar open={true}
+                      key={notification.id}
+                      autoHideDuration={5000}
+                      onClose={() => handleClose(notification.id)}>
+              <Alert severity={notification.type} variant="filled" onClose={() => handleClose(notification.id)}>
+                {notification.text}
+              </Alert>
+            </Snackbar>
+          ))}
+      </div>
+    }
   </NotificationContext.Provider>
 }
