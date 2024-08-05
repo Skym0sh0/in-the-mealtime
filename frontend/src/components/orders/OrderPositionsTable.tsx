1a7cd0c6a71509eaa1c45a49aff4f0b41b5d0298
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-08-06 00:25:20 +0200

    feat: #32: Styled new Table

--- frontend/src/components/orders/OrderPositionsTable.tsx
+++ frontend/src/components/orders/OrderPositionsTable.tsx
@@ -7,6 +7,8 @@ import {OrderPosition, OrderStateType} from "../../../build/generated-ts/api/api
 import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 import {DataGrid, GridColDef} from "@mui/x-data-grid";
 import {useMemo} from "react";
+import {darken, lighten, styled} from '@mui/material/styles';
+import {GridRowClassNameParams} from "@mui/x-data-grid/models/params";
 
 type TableRow = OrderPosition & {
   tableIndex: number,
@@ -76,18 +78,41 @@ export default function OrderPositionsTable({
         align: 'center',
         flex: 0.1,
         valueGetter: (_, row) => row.index + 1,
-        renderCell: (row) => <IndexCell position={row.row} selected={row.row.isSelected}/>,
+        renderCell: (row) => <div><IndexCell position={row.row} selected={row.row.isSelected}/></div>,
       },
-      {headerName: "Name", field: 'name', flex: 0.6,},
-      {headerName: "Gericht", field: 'meal', flex: 1,},
-      {headerName: "Preis", field: 'price', flex: 0.25, valueGetter: (_, row) => formatMonetaryAmount(row.price)},
-      {headerName: "Bezahlt", field: 'paid', flex: 0.25, valueGetter: (_, row) => formatMonetaryAmount(row.paid)},
-      {headerName: "Trinkgeld", field: 'tip', flex: 0.25, valueGetter: (_, row) => formatMonetaryAmount(row.tip)},
+      {headerName: "Name", field: 'name', flex: 0.6, align: 'left'},
+      {headerName: "Gericht", field: 'meal', flex: 1, align: 'left'},
+      {
+        headerName: "Preis",
+        field: 'price',
+        valueFormatter: formatMonetaryAmount,
+        flex: 0.25,
+        headerAlign: 'right',
+        align: 'right',
+      },
+      {
+        headerName: "Bezahlt",
+        field: 'paid',
+        valueFormatter: formatMonetaryAmount,
+        flex: 0.25,
+        headerAlign: 'right',
+        align: 'right',
+      },
+      {
+        headerName: "Trinkgeld",
+        field: 'tip',
+        valueFormatter: formatMonetaryAmount,
+        flex: 0.25,
+        headerAlign: 'right',
+        align: 'right',
+      },
       {
         headerName: "Rückgeld",
-        field: 'return',
+        field: 'returnMoney',
+        valueFormatter: formatMonetaryAmount,
         flex: 0.25,
-        valueGetter: (_, row) => formatMonetaryAmount(row.returnMoney)
+        headerAlign: 'right',
+        align: 'right',
       },
       {
         headerName: "Actions",
@@ -114,12 +139,17 @@ export default function OrderPositionsTable({
                 maxHeight: `${height}px`,
                 border: '1px solid lightgray'
               }}>
-    <DataGrid
-      pageSizeOptions={[100]}
-      columns={columns}
-      rows={rows}
-      rowHeight={32}
-      disableColumnMenu={true}
+    <StyledDataGrid pageSizeOptions={[100]}
+                    columns={columns}
+                    rows={rows}
+                    hideFooter={true}
+                    getRowClassName={(row: GridRowClassNameParams<TableRow>) => {
+                      if (!row.row.isErroneous)
+                        return "row-style-normal";
+                      return "row-style-error"
+                    }}
+                    rowHeight={32}
+                    disableColumnMenu={true}
     />
   </Box>
 }
@@ -170,3 +200,37 @@ function ActionCell({row, onSelect, onDeselect, onDelete}: {
     }
   </div>
 }
+
+const getBackgroundColor = (color: string, mode: string) => mode === 'dark' ? darken(color, 0.7) : lighten(color, 0.7);
+const getHoverBackgroundColor = (color: string, mode: string) => mode === 'dark' ? darken(color, 0.6) : lighten(color, 0.6);
+const getSelectedBackgroundColor = (color: string, mode: string) => mode === 'dark' ? darken(color, 0.5) : lighten(color, 0.5);
+const getSelectedHoverBackgroundColor = (color: string, mode: string) => mode === 'dark' ? darken(color, 0.4) : lighten(color, 0.4);
+
+const StyledDataGrid = styled(DataGrid)(({theme}) => {
+  return ({
+    '& .row-style-error': {
+      border: `3px solid ${getBackgroundColor(theme.palette.error.main, theme.palette.mode)}`,
+      '&:hover': {
+        backgroundColor: getHoverBackgroundColor(
+          theme.palette.warning.main,
+          theme.palette.mode,
+        ),
+      },
+      '&.Mui-selected': {
+        backgroundColor: getSelectedBackgroundColor(
+          theme.palette.warning.main,
+          theme.palette.mode,
+        ),
+        '&:hover': {
+          backgroundColor: getSelectedHoverBackgroundColor(
+            theme.palette.warning.main,
+            theme.palette.mode,
+          ),
+        },
+      },
+    },
+    '& .row-style-normal:nth-of-type(even)': {
+      backgroundColor: getBackgroundColor(theme.palette.grey["200"], theme.palette.mode),
+    }
+  });
+});
