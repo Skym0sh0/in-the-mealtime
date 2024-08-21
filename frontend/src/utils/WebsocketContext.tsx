4f1220e653ee1614095483688587e9c77888e72c
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-08-21 09:56:37 +0200

    feat: #9: Added Notification of Websockets Failed

--- frontend/src/utils/WebsocketContext.tsx
+++ frontend/src/utils/WebsocketContext.tsx
@@ -1,6 +1,7 @@
-import {createContext, ReactNode, useContext} from "react";
-import useWebSocket from "react-use-websocket";
+import {createContext, ReactNode, useContext, useEffect} from "react";
+import useWebSocket, {ReadyState} from "react-use-websocket";
 import {WebSocketHook} from "react-use-websocket/dist/lib/types";
+import {useNotification} from "./NotificationContext.tsx";
 
 export interface WebsocketContextType {
   ws?: WebSocketHook;
@@ -12,6 +13,21 @@ export const useWebsocketContext = () => useContext(WebsocketContext);
 
 export function WebsocketContextProvider({children}: { children?: ReactNode }) {
   const ws: WebSocketHook = useWebSocket(`${import.meta.env.VITE_APP_CONFIG_BACKEND_URL}/websocket`);
+
+  const {notifyError} = useNotification();
+
+  useEffect(() => {
+    const connectionStatus = {
+      [ReadyState.CONNECTING]: 'Connecting',
+      [ReadyState.OPEN]: 'Open',
+      [ReadyState.CLOSING]: 'Closing',
+      [ReadyState.CLOSED]: 'Closed',
+      [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+    }[ws.readyState];
+
+    if (![ReadyState.OPEN, ReadyState.CONNECTING].includes(ws.readyState))
+      notifyError(`Websocket Verbindung konnte nicht hergestellt werden: ${connectionStatus}`);
+  }, [ws.readyState, notifyError]);
 
   return <WebsocketContext.Provider value={{ws: ws}}>
     {children}
