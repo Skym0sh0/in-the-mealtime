0252fb389d36432f7e50e088e88478f7bb441925
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-09-02 14:54:41 +0200

    feat: #43: Added Frontend for Order Fees

--- frontend/src/components/orders/useOrderPositionSummary.ts
+++ frontend/src/components/orders/useOrderPositionSummary.ts
@@ -3,36 +3,45 @@ import {useMemo} from "react";
 import {Order, OrderPosition} from "../../../build/generated-ts/api/api.ts";
 
 export interface OrderSummaryType {
-  participants: number;
+    participants: number;
 
-  count: number;
-  price: number;
-  paid: number;
-  tip: number;
+    count: number;
+    price: number;
+    paid: number;
+    tip: number;
 
-  countMissing: number;
-  paidMissing: number;
-  changeMoney: number;
+    countMissing: number;
+    paidMissing: number;
+    changeMoney: number;
+
+    feeIsSatisfied: boolean;
 }
 
 export default function useOrderPositionSummary(order: Order): OrderSummaryType {
-  return useMemo(() => {
-    const add = (mapper: (pos: OrderPosition) => number, filter: (pos: OrderPosition) => boolean = () => true) => {
-      return order.orderPositions.filter(filter)
-        .map(mapper)
-        .reduce((agg, cur) => agg + cur, 0)
-    };
+    return useMemo(() => {
+        const add = (mapper: (pos: OrderPosition) => number, filter: (pos: OrderPosition) => boolean = () => true) => {
+            return order.orderPositions.filter(filter)
+                .map(mapper)
+                .reduce((agg, cur) => agg + cur, 0)
+        };
 
-    return {
-      participants: new Set(order.orderPositions.map(s => s.name)).size,
-      count: order.orderPositions.length,
-      price: add(l => l.price),
-      paid: add(l => l.paid ?? 0),
-      tip: add(l => l.tip ?? 0),
+        const fee = order.infos.orderFee ?? 0;
+        const tips = add(l => l.tip ?? 0);
+        const remainingFee = Math.max(fee - tips, 0);
+        const remainingTips = Math.max(tips - fee, 0);
 
-      countMissing: order.orderPositions.filter(pos => !pos.paid).length,
-      paidMissing: add(pos => pos.price, pos => !pos.paid),
-      changeMoney: add(pos => pos.paid ? pos.paid - pos.price - (pos.tip ?? 0) : 0),
-    };
-  }, [order.orderPositions])
+        return {
+            participants: new Set(order.orderPositions.map(s => s.name)).size,
+            count: order.orderPositions.length,
+            price: add(l => l.price) + fee,
+            paid: add(l => l.paid ?? 0),
+            tip: remainingTips,
+
+            countMissing: order.orderPositions.filter(pos => !pos.paid).length,
+            paidMissing: add(pos => pos.price, pos => !pos.paid) + remainingFee,
+            changeMoney: add(pos => pos.paid ? pos.paid - pos.price - (pos.tip ?? 0) : 0),
+
+            feeIsSatisfied: remainingFee <= 0,
+        };
+    }, [order.infos.orderFee, order.orderPositions])
 }
