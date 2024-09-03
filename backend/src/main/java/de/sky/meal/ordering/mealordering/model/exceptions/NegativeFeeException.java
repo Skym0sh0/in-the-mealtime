3e6d7c173b165d416c461f75efff9419834cbe24
fbieler <61547179+fbieler@users.noreply.github.com>
2024-09-03 13:10:34 +0200

    Integers for monetary values (in cent)

--- backend/src/main/java/de/sky/meal/ordering/mealordering/model/exceptions/NegativeFeeException.java
+++ backend/src/main/java/de/sky/meal/ordering/mealordering/model/exceptions/NegativeFeeException.java
@@ -1,7 +1,7 @@
 package de.sky.meal.ordering.mealordering.model.exceptions;
 
 public class NegativeFeeException extends MealtimeException {
-    public NegativeFeeException(String message, float fee) {
-        super("Negative fee is invalid", "%s. Fee was %f".formatted(message, fee));
+    public NegativeFeeException(String message, int fee) {
+        super("Negative fee is invalid", "%s. Fee was %.2f".formatted(message, fee / 100.0));
     }
 }
