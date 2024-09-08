45fcfab0bbdc2625ad69ebabb05594c2055a9bdb
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-09-08 14:03:41 +0200

    feat: #10: Added Infos to GUI

--- frontend/src/components/orders/OrderSummary.tsx
+++ frontend/src/components/orders/OrderSummary.tsx
@@ -14,43 +14,51 @@ export default function OrderSummary({order}: { order: Order }) {
     <TextField size="small"
                disabled={true}
                label="Teilnehmer"
+               title="So viele verschiedene Teilnehmer machen in dieser Bestellung mit."
                value={`${overallSum.participants} Teilnehmer`}/>
 
     <TextField size="small"
                disabled={true}
                label="Anzahl"
+               title="So viele Gerichte sind in dieser Bestellung enthalten."
                value={`${overallSum.count} Gerichte`}/>
 
     <TextField size="small"
                disabled={!overallSum.countMissing}
                label="Bezahlung fehlt für"
+               title="So viele Gerichte wurden bisher noch nicht als bezahlt abgehakt."
                error={!!overallSum.countMissing}
                value={`${overallSum.countMissing} Gericht${overallSum.countMissing !== 1 ? 'e' : ''}`}/>
 
     <TextField size="small"
                disabled={true}
                label="Summe"
+               title="Das sind die gesamten Kosten für diese Bestellung inklusive Lieferung."
                value={formatMonetaryAmount(overallSum.price)}/>
 
     <TextField size="small"
                disabled={true}
                label="Bezahlt"
+               title="Das ist der Betrag, der bisher gezahlt bzw. gesammelt wurde, inklusive Trinkgeld und Wechselgeld."
                value={formatMonetaryAmount(overallSum.paid)}/>
 
     <TextField size="small"
                disabled={!overallSum.paidMissing}
                label="Bezahlung fehlt"
+               title="Das ist der Betrag, der bisher noch fehlt, um die Bestellung bezahlen zu können."
                error={!!overallSum.paidMissing}
                value={formatMonetaryAmount(overallSum.paidMissing)}/>
 
     <TextField size="small"
                disabled={true}
                label="Rückgeld"
+               title="Das ist der Betrag, der zuviel gezahlt wurde und als Wechselgeld wieder an die Teilnehmer zurückgeht."
                value={formatMonetaryAmount(overallSum.changeMoney)}/>
 
     <TextField size="small"
                disabled={true}
                label="Trinkgeld"
+               title="Das ist der Betrag, der als Trinkgeld an das Restaurant gehen soll. Hiervon wird auch die Liefergebühr bzw. die Bestellkosten bezahlt."
                value={formatMonetaryAmount(overallSum.tip)}/>
   </Stack>;
 }
